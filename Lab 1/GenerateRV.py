96498c7c896b7efb4812c7959328d6247138ae4e
Daren Wang <d227wang@uwaterloo.ca>
2019-01-25 15:52:32 -0500

    Add python RV generation

--- Lab 1/GenerateRV.py
+++ Lab 1/GenerateRV.py
@@ -1,3 +1,17 @@
 import numpy as np
 import random
 import math
+
+def main():
+    lam = 75
+
+    expRV = []
+    for i in range(1000):
+    	expRV.append(float(-math.log(1 - random.uniform(0, 1))) / lam)
+
+    print ("Mean: %f", (sum(expRV) / len(expRV)))
+    print ("Variance: %f", np.var(expRV))
+
+
+if __name__ == '__main__':
+    main()
